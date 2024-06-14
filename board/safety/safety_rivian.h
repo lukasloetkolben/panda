14912770978a66d14fba39814b60a48d4db5a381
lukasloetkolben <61192133+lukasloetkolben@users.noreply.github.com>
2024-06-14 11:04:18 +0200

    rivian safety WIP

--- board/safety/safety_rivian.h
+++ board/safety/safety_rivian.h
@@ -19,20 +19,18 @@ const LongitudinalLimits RIVIAN_LONG_LIMITS = {
 const int FLAG_RIVIAN_LONG_CONTROL = 1;
 
 const CanMsg RIVIAN_TX_MSGS[] = {
-  {0x488, 0, 4},  // DAS_steeringControl
-  {0x2b9, 0, 8},  // DAS_control
-  {0x229, 1, 3},  // SCCM_rightStalk
+  {0x110, 0, 8},  // ACM_SteeringControl
+  {0x160, 0, 4},  // ACM_longitudinalRequest
 };
 
 RxCheck rivian_rx_checks[] = {
-  {.msg = {{0x2b9, 2, 8, .frequency = 25U}, { 0 }, { 0 }}},   // DAS_control
-  {.msg = {{0x155, 0, 8, .frequency = 50U}, { 0 }, { 0 }}},   // ESP_wheelRotation (speed in kph)
-  {.msg = {{0x370, 0, 8, .frequency = 100U}, { 0 }, { 0 }}},  // EPAS3S_internalSAS (steering angle)
-  {.msg = {{0x118, 0, 8, .frequency = 100U}, { 0 }, { 0 }}},  // DI_systemStatus (gas pedal)
-  {.msg = {{0x39d, 0, 5, .frequency = 25U}, { 0 }, { 0 }}},   // IBST_status (brakes)
-  {.msg = {{0x286, 0, 8, .frequency = 10U}, { 0 }, { 0 }}},   // DI_state (acc state)
-  {.msg = {{0x311, 0, 7, .frequency = 10U}, { 0 }, { 0 }}},   // UI_warning (buckle switch & doors)
-  {.msg = {{0x3f5, 1, 8, .frequency = 10U}, { 0 }, { 0 }}},   // ID3F5VCFRONT_lighting (blinkers)
+  {.msg = {{0x390, 0, 7, .frequency = 100U}, { 0 }, { 0 }}},  // EPAS_AdasStatus (steering angle)
+  {.msg = {{0x38b, 0, 4, .frequency = 50U}, { 0 }, { 0 }}},   // ESPiB1 (speed)
+  {.msg = {{0x150, 0, 7, .frequency = 50U}, { 0 }, { 0 }}},   // VDM_PropStatus (gas pedal)
+  {.msg = {{0x38f, 0, 6, .frequency = 50U}, { 0 }, { 0 }}},   // iBESP2 (brakes)
+  {.msg = {{0x162, 0, 6, .frequency = 100U}, { 0 }, { 0 }}},  // VDM_AdasSts (cruise state)
+  {.msg = {{0x101, 2, 8, .frequency = 100U}, { 0 }, { 0 }}},  // ACM_AebRequest (aeb)
+  {.msg = {{0x160, 2, 5, .frequency = 100U}, { 0 }, { 0 }}},  // ACM_longitudinalRequest (cruise control)
 };
 
 bool rivian_longitudinal = false;
@@ -42,52 +40,49 @@ static void rivian_rx_hook(const CANPacket_t *to_push) {
   int bus = GET_BUS(to_push);
   int addr = GET_ADDR(to_push);
 
-  if((addr == 0x370) && (bus == 0)) {
-    // Steering angle: (0.1 * val) - 819.2 in deg.
-    // Store it 1/10 deg to match steering request
-    int angle_meas_new = (((GET_BYTE(to_push, 4) & 0x3FU) << 8) | GET_BYTE(to_push, 5)) - 8192U;
-    update_sample(&angle_meas, angle_meas_new);
-  }
-
   if(bus == 0) {
-    if(addr == 0x155){
-      // Vehicle speed: (val * 0.5) * KPH_TO_MPS
-      float speed = ((((GET_BYTE(to_push, 6) & 0x0FU) << 6) | (GET_BYTE(to_push, 5) >> 2)) * 0.5) * 0.277778;
+
+    // Steering angle
+    if(addr == 0x390) {
+      // Angle: (0.1 * val) - 819.2 in deg.
+      // Store it 1/10 deg to match steering request
+      int angle_meas_new = ((GET_BYTE(to_push, 5) << 6) | (GET_BYTE(to_push, 6) >> 2)) - 8192U;
+      update_sample(&angle_meas, angle_meas_new);
+    }
+
+    // Vehicle speed
+    if(addr == 0x38b){
+      float speed = (GET_BYTE(to_push, 2)  * 0.4);
       vehicle_moving = ABS(speed) > 0.1;
       UPDATE_VEHICLE_SPEED(speed);
     }
 
     // Gas pressed
-    if(addr == 0x118){
-      gas_pressed = (GET_BYTE(to_push, 4) != 0U);
+    if(addr == 0x150){
+      gas_pressed = (((GET_BYTE(to_push, 3) << 2) | (GET_BYTE(to_push, 4) >> 6)) != 0U);
     }
 
     // Brake pressed
-    if(addr == 0x39d){
-      brake_pressed = (GET_BYTE(to_push, 2) & 0x03U)  == 2U;
+    if(addr == 0x38f){
+      brake_pressed = GET_BIT(to_push, 23U);
     }
 
     // Cruise state
-    if(addr == 0x286) {
-      int cruise_state = ((GET_BYTE(to_push, 1) << 1 ) >> 5);
-      bool cruise_engaged = (cruise_state == 2) ||  // ENABLED
-                            (cruise_state == 3) ||  // STANDSTILL
-                            (cruise_state == 4) ||  // OVERRIDE
-                            (cruise_state == 6) ||  // PRE_FAULT
-                            (cruise_state == 7);    // PRE_CANCEL
+    if(addr == 0x162) {
+      bool cruise_engaged = (((GET_BYTE(to_push, 2) & 0x18U) >> 3) == 1U);
       pcm_cruise_check(cruise_engaged);
     }
   }
 
   if (bus == 2) {
-    if (rivian_longitudinal && (addr == 0x2b9)) {
+    if (rivian_longitudinal && (addr == 0x101)) {
       // "AEB_ACTIVE"
-      rivian_stock_aeb = ((GET_BYTE(to_push, 2) & 0x03U) == 1U);
+      rivian_stock_aeb = GET_BIT(to_push, 47U);
     }
   }
 
-  generic_rx_checks((addr == 0x488) && (bus == 0));
-  generic_rx_checks((addr == 0x2b9) && (bus == 0));
+  generic_rx_checks((addr == 0x110) && (bus == 0));
+  generic_rx_checks((addr == 0x160) && (bus == 0));
 }
 
 
@@ -96,37 +91,21 @@ static bool rivian_tx_hook(const CANPacket_t *to_send) {
   int addr = GET_ADDR(to_send);
   bool violation = false;
 
-  if(addr == 0x488) {
-    // Steering control: (0.1 * val) - 1638.35 in deg.
+  // Steering control
+  if(addr == 0x110) {
+    // angle: (0.1 * val) - 1638.35 in deg.
     // We use 1/10 deg as a unit here
-    int raw_angle_can = (((GET_BYTE(to_send, 0) & 0x7FU) << 8) | GET_BYTE(to_send, 1));
+    int raw_angle_can = (((GET_BYTE(to_send, 2)) << 7) | ((GET_BYTE(to_send, 3) & 0xfeU) >> 1));
     int desired_angle = raw_angle_can - 16384;
-    int steer_control_type = GET_BYTE(to_send, 2) >> 6;
-    bool steer_control_enabled = (steer_control_type != 0) &&  // NONE
-                                 (steer_control_type != 3);    // DISABLED
-
+    bool steer_control_enabled = ((GET_BYTE(to_send, 1) & 0x30U) >> 4);
     if (steer_angle_cmd_checks(desired_angle, steer_control_enabled, RIVIAN_STEERING_LIMITS)) {
       violation = true;
     }
   }
 
-
-  if (addr == 0x229){
-    // Only the "Half up" and "Neutral" positions are permitted for sending stalk signals.
-    int control_lever_status = ((GET_BYTE(to_send, 1) & 0x70U) >> 4);
-    if ((control_lever_status > 1)) {
-      violation = true;
-    }
-  }
-
-  if(addr == 0x2b9) {
-    // DAS_control: longitudinal control message
+  // Longitudinal control
+  if(addr == 0x160) {
     if (rivian_longitudinal) {
-      // No AEB events may be sent by openpilot
-      int aeb_event = GET_BYTE(to_send, 2) & 0x03U;
-      if (aeb_event != 0) {
-        violation = true;
-      }
 
       // Don't send messages when the stock AEB system is active
       if (rivian_stock_aeb) {
@@ -134,11 +113,17 @@ static bool rivian_tx_hook(const CANPacket_t *to_send) {
       }
 
       // Don't allow any acceleration limits above the safety limits
-      int raw_accel_max = ((GET_BYTE(to_send, 6) & 0x1FU) << 4) | (GET_BYTE(to_send, 5) >> 4);
-      int raw_accel_min = ((GET_BYTE(to_send, 5) & 0x0FU) << 5) | (GET_BYTE(to_send, 4) >> 3);
-      violation |= longitudinal_accel_checks(raw_accel_max, RIVIAN_LONG_LIMITS);
-      violation |= longitudinal_accel_checks(raw_accel_min, RIVIAN_LONG_LIMITS);
+      int raw_accel = (GET_BYTE(to_send, 2) << 3) | (GET_BYTE(to_send, 3) >> 5);
+      violation |= longitudinal_accel_checks(raw_accel, RIVIAN_LONG_LIMITS);
     } else {
+      violation = true;
+    }
+  }
+
+  if (addr == 0x229){ // todo button cmds
+    // Only the "Half up" and "Neutral" positions are permitted for sending stalk signals.
+    int control_lever_status = ((GET_BYTE(to_send, 1) & 0x70U) >> 4);
+    if ((control_lever_status > 1)) {
       violation = true;
     }
   }
@@ -154,17 +139,16 @@ static int rivian_fwd_hook(int bus_num, int addr) {
   int bus_fwd = -1;
 
   if(bus_num == 0) {
-    // Party to autopilot
     bus_fwd = 2;
   }
 
   if(bus_num == 2) {
     bool block_msg = false;
-    if (addr == 0x488) {
+    if (addr == 0x110) {
       block_msg = true;
     }
 
-    if (rivian_longitudinal && (addr == 0x2b9) && !rivian_stock_aeb) {
+    if (rivian_longitudinal && (addr == 0x160) && !rivian_stock_aeb) {
       block_msg = true;
     }
 
